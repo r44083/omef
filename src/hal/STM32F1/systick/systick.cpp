c5c8037a0c769e5c6670f4fb7360d39c51fa6640
r44083 <r44083.r@gmail.com>
2020-05-03 16:33:59 +0300

    Refactoring hal systick to make it like static class

--- src/hal/STM32F1/systick/systick.cpp
+++ src/hal/STM32F1/systick/systick.cpp
@@ -7,18 +7,18 @@
 
 using namespace hal;
 
-void hal::systick_init(void)
+void systick::init(void)
 {
 	uint32_t systick_freq = rcc_get_freq(RCC_SRC_AHB);
 	SysTick_Config(systick_freq / configTICK_RATE_HZ);
 }
 
-uint32_t hal::systick_get_ms(void)
+uint32_t systick::get_ms(void)
 {
 	return xTaskGetTickCount();
 }
 
-uint32_t hal::systick_get_past(uint32_t start)
+uint32_t systick::get_past(uint32_t start)
 {
 	return xTaskGetTickCount() - start;
 }
