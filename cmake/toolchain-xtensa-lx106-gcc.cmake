b29f69f6a690c96153fdabd6a3a537beca585022
yhsb2k <yhsb2k@outlook.com>
2021-04-25 18:31:46 +0300

    Remove unneded option from esp8266 toolchain file

--- cmake/toolchain-xtensa-lx106-gcc.cmake
+++ cmake/toolchain-xtensa-lx106-gcc.cmake
@@ -1,7 +1,6 @@
 find_program(CMAKE_C_COMPILER NAMES xtensa-lx106-elf-gcc)
 find_program(CMAKE_CXX_COMPILER NAMES xtensa-lx106-elf-g++)
 find_program(CMAKE_ASM_COMPILER NAMES xtensa-lx106-elf-gcc)
-set(CMAKE_ASM_FLAGS "-x assembler-with-cpp")
 find_program(CMAKE_OBJCOPY NAMES xtensa-lx106-elf-objcopy)
 find_program(CMAKE_SIZE NAMES xtensa-lx106-elf-size)
 
